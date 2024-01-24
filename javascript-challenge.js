fefb4a3a196c26e19d32f03feaa5506150c9cb63
Athish Thayalan <tathish@hotmail.co.uk>
2024-01-24 13:19:33 +0000

    rewrote the getLargestNumber function to not take an array as the arguemnt , but to take 3 seperate integers

--- javascript-challenge.js
+++ javascript-challenge.js
@@ -2,24 +2,23 @@ function isNumberPositive(n) {
   return n > 0;
 }
 
-function convertDaysToAge(n){
-  return Math.floor(n/365)
+function convertDaysToAge(n) {
+  return Math.floor(n / 365);
 }
 
-function getLargestNumber(arr){
-  if(arr.length === 0){
-    return NaN
+function getLargestNumber(a, b, c) {
+  max = a;
+  if (b > max) {
+    max = b;
   }
-  let max = arr[0]
-  for(let i=1 ; i<arr.length;i++){
-    if(arr[i]>max){
-      max = arr[i]
-    }
+
+  if(c>max){
+    max =c
   }
-  return max;
+
+  return max
 }
 
-console.log(getLargestNumber([]))
-
-
-
+function getLastName() {
+  return;
+}
