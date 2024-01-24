02cdcb4b85c39c349cccbcd772170748ca438bd7
Athish Thayalan <tathish@hotmail.co.uk>
2024-01-24 13:16:11 +0000

    Made a function which takes an array as an arguments and finds the highest number in the array using a for loop

--- javascript-challenge.js
+++ javascript-challenge.js
@@ -6,5 +6,20 @@ function convertDaysToAge(n){
   return Math.floor(n/365)
 }
 
+function getLargestNumber(arr){
+  if(arr.length === 0){
+    return NaN
+  }
+  let max = arr[0]
+  for(let i=1 ; i<arr.length;i++){
+    if(arr[i]>max){
+      max = arr[i]
+    }
+  }
+  return max;
+}
+
+console.log(getLargestNumber([]))
+
 
 
